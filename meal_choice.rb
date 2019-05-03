029cb68ab7cb217693c6595c9e559acc2c80713a
voluble-desk-1675 <antonio.blandin25@gmail.com>
2019-05-03 16:47:41 +0000

    Done.

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,11 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
+
+
+def meal_choice(meat = "meat")
+  puts "what kind of #{meat} would you like?"
+end
+
+meal_choice
+
+  
